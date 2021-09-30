47272264aabb2cfdbc2629654d31ea8bb9f97a6e
y4shiro <y4shironao@gmail.com>
2021-09-30 18:59:43 +0900

    ESLint 導入

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  eslint: {
+    dirs: ['src/components/', 'src/lib/', 'src/pages/'],
+    ignoreDuringBuilds: true,
+  },
+};
