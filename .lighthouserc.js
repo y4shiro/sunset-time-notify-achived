91a138204da35bf63ac5ce55b325b6ead3ccc69c
y4shiro <y4shironao@gmail.com>
2021-10-01 16:09:09 +0900

    Lighthouse CI Server のリンクを追加

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -6,7 +6,9 @@ module.exports = {
       url: ['/'],
     },
     upload: {
-      target: 'temporary-public-storage',
+      target: 'lhci',
+      serverBaseUrl: 'https://gentle-eyrie-01072.herokuapp.com',
+      token: 'e8ad80d2-c424-4f96-9211-337d1b10ef44',
     },
   },
 };
