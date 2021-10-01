4fdfbcc1f9f3f2ac358dfa621e161944051f5b65
y4shiro <y4shironao@gmail.com>
2021-10-01 10:12:57 +0900

    Chakra UI インストール

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
 import type { NextPage } from 'next';
 import Image from 'next/image';
 
+import React from 'react';
+import { Box, chakra } from '@chakra-ui/react';
+
 const Home: NextPage = () => {
-  return <h1>Sunset Time Notify</h1>;
+  return (
+    <>
+      <h1>Sunset Time Notify</h1>
+      <Box>
+        <chakra.h2 color='blue.600'>H2 要素に Chakra UI 適用</chakra.h2>
+      </Box>
+    </>
+  );
 };
 
 export default Home;
