ab80e476ab3b4aa7a6f80b0f4045f3664b931307
y4shiro <y4shironao@gmail.com>
2021-10-03 14:36:19 +0900

    Header を仮スタイリング

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import { Box, Flex, Heading, HStack, Spacer, Text } from '@chakra-ui/react';
 
 const Header: React.FC = () => {
   return (
     <>
-      <Heading bg='blue.400'>ヘッダー</Heading>
+      <Flex bg='blue.500' align='center' justify='space-between'>
+        <Heading p={4} color='white'>
+          Sunset Time Notify
+        </Heading>
+
+        <Spacer />
+
+        <HStack color='white' mr={4} gridGap='4'>
+          <Box>
+            <Text>About</Text>
+          </Box>
+          <Box>
+            <Text>Contact</Text>
+          </Box>
+        </HStack>
+      </Flex>
     </>
   );
 };
