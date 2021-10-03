289de23c34a7d1096d59207fa623b9c80755c741
y4shiro <y4shironao@gmail.com>
2021-10-03 14:39:38 +0900

    Footer を仮スタイリング

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import { Box, Heading } from '@chakra-ui/react';
 
 const Footer: React.FC = () => {
   return (
     <>
-      <Heading bg='gray.400'>フッター</Heading>
+      <Box bg='gray.600' p='8' textAlign='center'>
+        <Heading color='white' fontSize='md'>
+          &copy; 2021 y4shiro.
+        </Heading>
+      </Box>
     </>
   );
 };
