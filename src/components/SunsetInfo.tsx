5f6f9dfece22680de9d8c2b1033b7f4c975676b3
y4shiro <y4shironao@gmail.com>
2021-10-04 11:30:04 +0900

    Form に入力した値を localStorage に保存、ページ読み込み時は localStorage の値を Form に反映

--- src/components/SunsetInfo.tsx
+++ src/components/SunsetInfo.tsx
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Heading, Input, Text, Stack } from '@chakra-ui/react';
 
 const SunsetInfo: React.FC = () => {
   const [formValue, setFormValue] = useState('');
 
   const handleChange = (e: { target: { value: React.SetStateAction<string> } }) => setFormValue(e.target.value);
+
+  // ページ読み込み時に localStorage から値を取得して useState に保存
+  useEffect(() => {
+    const localStorageValue = JSON.parse(localStorage.getItem('form'));
+    setFormValue(localStorageValue);
+  }, []);
+
+  // formValue の値を更新した場合は localStorage にも反映させる
+  useEffect(() => {
+    localStorage.setItem('form', JSON.stringify(formValue));
+  }, [formValue]);
 
   return (
     <Stack m='4' spacing={4}>
