00eb4797610799067c53c4f9e928e7f85a2e7507
y4shiro <y4shironao@gmail.com>
2021-10-06 14:20:37 +0900

    LocalStorage に値が無い場合はデフォルト値を保存する

--- src/hooks/usePrefNum.ts
+++ src/hooks/usePrefNum.ts
@@ -1,19 +1,25 @@
 import { useState, useEffect } from 'react';
 
+const defautlPrefNum = 0; // 0 は北海道を指す
+
 const usePrefNum = () => {
-  const [prefNum, setPrefNum] = useState(0);
+  const [prefNum, setPrefNum] = useState(defautlPrefNum);
 
   // ページ読み込み時に localStorage から値を取得して useState に保存
   useEffect(() => {
-    const localStorageValue = JSON.parse(localStorage.getItem('prefNum')!);
-    if (localStorageValue) setPrefNum(localStorageValue);
-    else setPrefNum(0);
+    const localStorageValue = localStorage.getItem('prefNum');
+
+    if (localStorageValue) {
+      setPrefNum(JSON.parse(localStorageValue));
+    } else {
+      localStorage.setItem('prefNum', JSON.stringify(defautlPrefNum));
+      setPrefNum(defautlPrefNum);
+    }
   }, []);
 
   const changePrefNum = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const num = parseInt(e.target.value);
     setPrefNum(num);
-    console.log('exec changePrefNum:', num);
 
     // prefNum の値を更新した場合は localStorage にも反映させる
     localStorage.setItem('prefNum', JSON.stringify(num));
