a2cbff8e71bbcd55493273f4ee25302a699f85d4
y4shiro <y4shironao@gmail.com>
2021-10-11 11:31:58 +0900

    関数の型定義を独立させた

--- src/lib/calcSunTime.ts
+++ src/lib/calcSunTime.ts
@@ -1,14 +1,14 @@
 import { getSunrise, getSunset } from 'sunrise-sunset-js';
 
-const calcSunriseTime = (lat: number, lng: number): string => {
-  const sunriseTime = getSunrise(lat, lng);
+type SunTimeFunc = (lat: number, lng: number) => string;
 
+const calcSunriseTime: SunTimeFunc = (lat, lng) => {
+  const sunriseTime = getSunrise(lat, lng);
   return sunriseTime.toLocaleTimeString('en-GB');
 };
 
-const calcSunsetTime = (lat: number, lng: number): string => {
+const calcSunsetTime: SunTimeFunc = (lat, lng) => {
   const sunsetTime = getSunset(lat, lng);
-
   return sunsetTime.toLocaleTimeString('en-GB');
 };
 
