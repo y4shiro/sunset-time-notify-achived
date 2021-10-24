53d6f2e13728582e0fd28d1f1422c6c14784a9f8
y4shiro <y4shironao@gmail.com>
2021-10-24 18:09:13 +0900

    SunsetInfo/SunsetInfo.tsx を SunsetInfo/index.tsx にリネーム

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container, Box, Heading, HStack, SimpleGrid } from '@chakra-ui/react';
 
 import Map from './Map';
-import SunsetInfo from './SunsetInfo/SunsetInfo';
+import SunsetInfo from './SunsetInfo/Index';
 
 const MainContent: React.FC = () => {
   return (
