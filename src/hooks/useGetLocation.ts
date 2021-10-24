0b02222783c91d87455cc6695d33cc20106adce2
y4shiro <y4shironao@gmail.com>
2021-10-24 20:43:42 +0900

    getCurrentPosition の処理を非同期で行うよう変更し、loading のステートも追加

--- src/hooks/useGetLocation.ts
+++ src/hooks/useGetLocation.ts
@@ -1,23 +1,41 @@
 import { useState } from 'react';
 
+type positionType = {
+  coords: { latitude: number; longitude: number };
+};
+
 const useGetLocation = () => {
   const [lat, setLat] = useState(0);
   const [lng, setLng] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-  const getLocation = () => {
+  console.log(loading);
+
+  const getLocation = async () => {
     if (!navigator.geolocation) {
       console.log('Unable to retrieve your location');
     } else {
-      navigator.geolocation.getCurrentPosition(success, error);
+      try {
+        setLoading(true);
+        const location = await getCurrentPosition();
+        setLocations(location);
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setLoading(false);
+      }
     }
   };
 
-  const success = (position: { coords: { latitude: number; longitude: number } }) => {
+  const getCurrentPosition = (): Promise<positionType> => {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+  };
+
+  const setLocations = (position: positionType) => {
     setLat(position.coords.latitude);
     setLng(position.coords.longitude);
-  };
-  const error = (err: GeolocationPositionError) => {
-    console.log(err.message);
   };
 
   return { lat, lng, getLocation };
