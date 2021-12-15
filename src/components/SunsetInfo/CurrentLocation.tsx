884100cd2b7457cf268da0f184d79c3059409bb9
y4shiro <y4shironao@gmail.com>
2021-12-15 11:34:07 +0900

    現在位置の日出日没時刻を表示

--- src/components/SunsetInfo/CurrentLocation.tsx
+++ src/components/SunsetInfo/CurrentLocation.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Box, Button, Text } from '@chakra-ui/react';
 
+import { calcSunsetTime, calcSunriseTime } from '../../lib/calcSunTime';
 import useGetLocation from '../../hooks/useGetLocation';
 
 const CurrentLocation: React.FC = () => {
   const { lat, lng, loading, error, getLocation } = useGetLocation();
+
+  const sunriseTime: string = calcSunriseTime(lat, lng);
+  const sunsetTime: string = calcSunsetTime(lat, lng);
 
   return (
     <Box>
@@ -12,6 +16,8 @@ const CurrentLocation: React.FC = () => {
       <Button onClick={() => getLocation()}>現在地を取得する</Button>
       <Text>現在の経度:{loading ? '読込中...' : lat}</Text>
       <Text>現在の緯度:{loading ? '読込中...' : lng}</Text>
+      <Text>本日の日出時刻は{lat === 0 ? '00:00:00' : sunriseTime}です</Text>
+      <Text>本日の日没時刻は{lng === 0 ? '00:00:00' : sunsetTime}です</Text>
       <Text>エラー:{error}</Text>
     </Box>
   );
